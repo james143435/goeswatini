24f7d81999e62bf9dc29d646dfe87340cd94556c
mosaic141688 <mbhamalimosaic@gmail.com>
2019-05-01 19:45:39 +0200

    initial commit

---
+++ app/src/main/java/com/goeswatini/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,37 @@
+package com.goeswatini.ui.main
+
+import android.content.Context
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.goeswatini.R
+
+private val TAB_TITLES = arrayOf(
+"HOTELS",
+"RESTAURANT",
+"MUESUM",
+"Supermarket",
+"Police station"
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return TAB_TITLES[position]//context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return TAB_TITLES.size
+    }
+}
